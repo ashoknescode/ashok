4508afad145ef422fd1c4b3c7a421a5898e8ea94
ashoknescode <ashok.kumar@nescode.com>
2018-12-29 05:00:34 +0530

    [Update] settings file

--- ashok/settings/base.py
+++ ashok/settings/base.py
@@ -162,3 +162,6 @@ WAGTAIL_SITE_NAME = "ashok"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+
+# Enable update message disable in admin dashboard
+WAGTAIL_ENABLE_UPDATE_CHECK = False
